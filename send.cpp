68a6e36560f94f5e62c2bb89d27435063e1e52f3
Lukas Rose <lukas.rose@gmx.de>
2015-06-23 23:46:25 +0200

    Added usage information and help text on errors, additional code documentation

--- send.cpp
+++ send.cpp
@@ -9,6 +9,17 @@
 #include <string>
 //#include <iostream>
 
+void printUsage(){
+  printf("This is rasperry remote, an application to control remote plugs with the Raspberry Pi.\n");
+  printf("Based on RCSwitch and wiringPi. See github.com/xkonni/raspberry-remote for further reference.\n");
+  printf("Usage: \n sudo send [-b] <systemCode> <unitCode> <command> or:\n sudo send -h\n");
+  printf("Where the -b argument switches the instance to binary mode, and the -h option displays this help.\n\n");
+  printf("Binary mode means, that instead numbering the sockets by 00001 00010 00100 01000 10000, the sockets\n");
+  printf("are numbered in real binary numbers as following: 00001 00010 00011 00100 00101 00110 and so on.\n");
+  printf("This means that your sockets need to be setup in this manner, which often includes that the dedicated remote\n");
+  printf("is rendered useless, but more than 6 sockets are supported.\n");
+}
+
 int main(int argc, char *argv[]) {
   /**
    * output PIN is hardcoded for testing purposes
@@ -24,15 +35,55 @@ int main(int argc, char *argv[]) {
   char* systemCode;
   int unitCode;
   int command;
+
+  if (argc < 4) {
+    /**
+     * no matter, which mode is used, at least 4 arguments are required:
+     * 0: command name (send)
+     * 1: systemCode
+     * 2: unitCode
+     * 3: command
+     * if there are less arguments, the help should be printed
+     * and the application should terminate.
+     */
+    printUsage();
+    return 1;
+  }
+
+  /**
+   * This needs to stand after the check of how many arguments are passed,
+   * because if there is only 1 argument passed, argv[1] is NULL, which
+   * will result in an error, because a std::string can't be constructed
+   * by a NULL value. Therefore it is important to terminate the application
+   * if there are less than 2 arguments (in this case: less than 4 arguments)
+   * passed.
+   */
   std::string firstArgument = argv[1];
 
-  if (firstArgument == "-b") {
+  if (firstArgument == "-b" or firstArgument == "--binary") {
+    if (argc < 5) {
+    /**
+     * in binaryMode, 5 arguments are required:
+     * 0: command name ('send')
+     * 1: binary operator ('-b')
+     * 2: systemCode
+     * 3: unitCode
+     * 4: command
+     * if there are less arguments, the help should be printed,
+     * and the application should terminate.
+     */
+      printUsage();
+      return 1;
+    }
+
     printf("operating in binary mode...\n");
     binaryMode = true;
     //position of data in input is now shifted by 1 because of '-b'-flag
     systemCode = argv[2];
     unitCode = atoi(argv[3]);
     command  = atoi(argv[4]);
+  } else if (firstArgument == "-h" or firstArgument == "--help" or firstArgument == "-?") {
+    printUsage();
   } else {
     //no binary mode, therefore using normal mode with old numbering
     systemCode = argv[1];
@@ -57,6 +108,7 @@ int main(int argc, char *argv[]) {
         break;
       default:
         printf("command[%i] is unsupported\n", command);
+        printUsage();
         return -1;
     }
     return 0;
@@ -78,6 +130,7 @@ int main(int argc, char *argv[]) {
         break;
       default:
         printf("command[%i] is unsupported\n", command);
+        printUsage();
         return -1;
     }
     return 0;
